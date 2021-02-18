e7fdca91efef7c474240a42353a72169acab478a
xx <xx@xx.com>
2021-02-18 10:54:36 +0800

    modify port

--- start.sh
+++ start.sh
@@ -2,9 +2,9 @@
 
 docker run -d --name hecomainnetnet1.1 \
            -v `pwd`:/root/hecomainnet \
-           -p 8545:8545 \
-           -p 30303:30303 \
-           -p 8546:8546 \
+           -p 8555:8545 \
+           -p 30313:30303 \
+           -p 8556:8546 \
            --restart=unless-stopped \
            hecofans/geth:1.1 \
            geth-linux-amd64 --config /root/hecomainnet/config.toml  \
